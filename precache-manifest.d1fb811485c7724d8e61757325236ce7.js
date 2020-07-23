d74df7bb7be823a0be2bf878fea75293ae113ff2
Avishka2019 <avishkadevinda2017@gmail.com>
2020-07-23 10:43:42 +0530

    Updates

---
+++ precache-manifest.d1fb811485c7724d8e61757325236ce7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "813db41eebe96fec7da7",
+    "url": "/my_api.github.io/static/css/main.8ed71275.chunk.css"
+  },
+  {
+    "revision": "813db41eebe96fec7da7",
+    "url": "/my_api.github.io/static/js/main.813db41e.chunk.js"
+  },
+  {
+    "revision": "9e257698b27d26f5992a",
+    "url": "/my_api.github.io/static/js/runtime~main.9e257698.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/my_api.github.io/static/js/2.3791395a.chunk.js"
+  },
+  {
+    "revision": "65574e1635c555c39c1b0806085ffb85",
+    "url": "/my_api.github.io/index.html"
+  }
+];
